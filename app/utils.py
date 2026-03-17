e9b333fc283a334bc2635b95e98feb73fc16c643
emzamike1997-lab <emzamike1997@gmail.com>
2026-03-17 16:13:53 +0100

    JAVAS: Add app/utils.py

--- app/utils.py
+++ app/utils.py
@@ -1,14 +1,11 @@
 ```python
-# Utility functions
+import logging
 
-def validate_todo(data):
-    """Validate the todo data"""
-    errors = []
-    if not data.get('title'):
-        errors.append('Title is required')
-    if not data.get('description'):
-        errors.append('Description is required')
-    return errors
+def setup_logging():
+    logging.basicConfig(level=logging.INFO)
+    logging.info('Logging setup')
+
+setup_logging()
 ```
 
 ###
