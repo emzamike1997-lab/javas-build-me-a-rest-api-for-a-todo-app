ff99a990ad15b9b5d5d14a810c0da46e035f47a1
emzamike1997-lab <emzamike1997@gmail.com>
2026-03-17 16:13:49 +0100

    JAVAS: Add app/models.py

--- app/models.py
+++ app/models.py
@@ -1,29 +1,23 @@
 ```python
-# Define the Todo model
+from flask_sqlalchemy import SQLAlchemy
+from marshmallow_sqlalchemy import SQLAlchemyAutoSchema
 
-from app import db
-from datetime import datetime
+db = SQLAlchemy()
 
 class Todo(db.Model):
-    """Todo model"""
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(100), nullable=False)
     description = db.Column(db.String(200), nullable=False)
     completed = db.Column(db.Boolean, default=False)
-    created_at = db.Column(db.DateTime, default=datetime.utcnow, nullable=False)
 
-    def __repr__(self):
-        return f'Todo({self.title}, {self.description}, {self.completed})'
+    def __init__(self, title, description):
+        self.title = title
+        self.description = description
 
-    def to_dict(self):
-        """Convert the Todo object to a dictionary"""
-        return {
-            'id': self.id,
-            'title': self.title,
-            'description': self.description,
-            'completed': self.completed,
-            'created_at': self.created_at
-        }
+class TodoSchema(SQLAlchemyAutoSchema):
+    class Meta:
+        model = Todo
+        load_instance = True
 ```
 
 ###
